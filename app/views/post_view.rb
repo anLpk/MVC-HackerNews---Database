c145cd3cbef02ed42957454c7266f5e20ec3abdf
Anil Pak <71243066+anLpk@users.noreply.github.com>
2020-11-16 16:11:07 +1100

    Update post_view.rb

--- app/views/post_view.rb
+++ app/views/post_view.rb
@@ -1,3 +1,31 @@
 class PostsView
-  # TODO: implement methods called by the PostsController
+  def title
+    puts "Give me the title"
+    print "> "
+    gets.chomp
+  end
+
+  def url
+    puts "Give me the URL"
+    print "> "
+    gets.chomp
+  end
+
+  def votes
+    puts "Give me the votes"
+    print "> "
+    gets.chomp.to_i
+  end
+
+  def id
+    puts "Give me a number"
+    print "> "
+    gets.chomp.to_i
+  end
+
+  def display(posts)
+    posts.each do |post|
+      puts "#{post.id} -\nTitle: #{post.title}\nURL: #{post.url}\nVotes: #{post.votes}\n\n"
+    end
+  end
 end
